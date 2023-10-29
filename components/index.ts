4953afffc9e00bbd5ca7431e052da1c5bed8664b
GonzaloSantos1 <gsantosorellana.dev@gmail.com>
2023-10-29 22:02:19 +0100

    final commit

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import CustomHeader from './CustomHeader'
+import BottomSheet from './BottomSheet'
+import Categories from './Categories'
+import Restaurants from './Restaurants'
+
+export { CustomHeader, BottomSheet, Categories, Restaurants }
